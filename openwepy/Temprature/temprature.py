db36be0a9b9f8507d3b4c67bee2f4e8c2a495172
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-04-22 21:43:29 +0300

    Update temprature.py

--- openwepy/Temprature/temprature.py
+++ openwepy/Temprature/temprature.py
@@ -11,7 +11,15 @@ class temprature:
         formula = ((kelvin - 273.15) * 9/5) + 32
         return formula
 
+    def fahrenheit_to_kelvin(fahrenheit):
+        formula = (fahrenheit - 32) * 5/9 + 273.15
+        return formula
+
     def kelvin_to_celcius(kelvin):
         fahrenheit = temprature.kelvin_to_fahrenheit(kelvin)
         formula = temprature.fahrenheit_to_celcius(fahrenheit)
+        return formula
+
+    def celcius_to_kelvin(celcius):
+        formula = celcius + 273.15
         return formula
